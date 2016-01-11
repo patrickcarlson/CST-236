fa69d92b05c691ce3cc603c2a07528bb023b311e
patrickcarlson <patrick.carlson@oit.edu>
2016-01-10 17:56:25 -0800

    Quadrilateral.
    Need double checking of methods in last two shape checking functions. Then also testing process

--- cst236_lab1/source/shape_checker.py
+++ cst236_lab1/source/shape_checker.py
@@ -48,40 +48,90 @@ def get_triangle_type(a=0, b=0, c=0):
     else:
         return "scalene"
 
-def get_quadrilateral_type(a=0, b=0, c=0, d=0):
-     """
-     :param a: line a
-     :type float or int or tuple or list or dict
+def get_squarerectangle_type(a=0, b=0, c=0, d=0):
+    """
+    :param a: line a
+    :type float or int
 
-     :param b:
-     :type float or int
+    :param b: line b
+    :type float or int
 
-     :param c:
-     :type float or int
+    :param c: line c
+    :type float or int
 
-     :param d:
-     :type float or int
+    :param d: line d
+    :type float or int
 
-     :return: str
-     """
+    :return: str
+    """
 
-     if isinstance(a (tuple, list)) and len(a) == 4:
-         d = a[3]
-         c = a[2]
-         b = a[1]
-         a = a[0]
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(d, (int, float))):
+        return "invalid"
 
-     if isinstance(a, dict) and len(a.keys()) == 4:
-        values = []
-        for value in a.values():
-            values.append(value)
-        a = values[0]
-        b = values[1]
-        c = values[2]
-        d = values[3]
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        return "invalid"
 
+    if a == b and b == c and c == d:
+        return "square"
 
+    if a == c and b == d:
+        return "rectangle"
 
+    else:
+        return "invalid"
 
+def get_quadrilateral_type(a=0, b=0, c=0, d=0, ab=0, bc=0, cd=0, da=0):
+    """
+    :param a: line 1
+    :type float or int
 
+    :param b: line 2
+    :type float or int
 
+    :param c: line 3
+    :type float or int
+
+    :param d: line 4
+    :type float or int
+
+    :param ab: angle ab
+    :type float or int
+
+    :param bc: angle bc
+    :type float or int
+
+    :param cd: angle cd
+    :type float or int
+
+    :param da: angle da
+    :type float or int
+
+    :return: str
+    """
+
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(d, (int, float))):
+        return 'invalid'
+
+    if not (isinstance(ab, (int, float)) and isinstance(bc, (int, float)) and isinstance(cd, (int, float)) and isinstance(da, (int, float))):
+        return 'invalid'
+
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        return 'invalid'
+
+    if ab <= 0 or bc <= 0 or cd <= 0 or da <= 0:
+        return 'invalid'
+
+    if ab >= 180 or bc >= 180 or cd >= 180 or da >= 180:
+        return 'invalid'
+
+    if a == b and b == c and c == d and ab == 90 and bc == 90 and cd == 90 and da == 90:
+        return 'square'
+
+    if a == c and b == d and ab == 90 and bc == 90 and cd == 90 and da == 90:
+        return 'rectangle'
+
+    if a == b and b == c and c == d and ab == cd and bc == da and ab + bc == 180:
+        return 'rhombus'
+
+    else:
+        return 'disconnected'
--- cst236_lab1/test/shape_checker_test.py
+++ cst236_lab1/test/shape_checker_test.py
@@ -1,13 +1,18 @@
 """
 Test for source.shape_checker
 """
-from source.shape_checker import get_triangle_type
+from source.shape_checker import get_triangle_type, get_squarerectangle_type, get_quadrilateral_type
 from unittest import TestCase
 
 class TestGetTriangleType(TestCase):
 
     def test_get_triangle_fromtuple_all_int(self):
         a = (3, 2, 1)
+        result = get_triangle_type(a)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_fromdict_all_int(self):
+        a = {'a': 1, 'b': 2, 'c': 3}
         result = get_triangle_type(a)
         self.assertEqual(result, 'scalene')
 
@@ -30,3 +35,63 @@ class TestGetTriangleType(TestCase):
     def test_get_triangle_nonintfloat_all_int(self):
         result = get_triangle_type('a', "DEADBEEF", 'Z')
         self.assertEqual(result, 'invalid')
+
+class TestGetSquareRectangleType(TestCase) :
+
+    def test_get_squarerectangle_lessthanzero_all_int(self):
+        result = get_squarerectangle_type(-1, -1 , -1, -1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_squarerectangle_nonintfloat_all_char(self):
+        result = get_squarerectangle_type('A', 'A', 'A', 'A')
+        self.assertEqual(result, 'invalid')
+
+    def test_get_squarerectangle_square_all_int(self):
+        result = get_squarerectangle_type(2, 2, 2, 2,)
+        self.assertEqual(result, 'square')
+
+    def test_get_squarerectangle_square_all_float(self):
+        result = get_squarerectangle_type(2.22, 2.22, 2.22, 2.22,)
+        self.assertEqual(result, 'square')
+
+    def test_get_squarerectangle_rectangle_all_int(self):
+        result = get_squarerectangle_type(1, 2, 1, 2)
+        self.assertEqual(result, 'rectangle')
+
+    def test_get_squarerectangle_wronglength_all_int(self):
+        result = get_squarerectangle_type(1, 2, 1, 3)
+        self.assertEqual(result, 'invalid')
+
+class TestGetQuadrilateralType(TestCase) :
+
+    def test_get_quadrilateral_sidelessthanzero_all_int(self):
+        result = get_quadrilateral_type(-1, -1, -1, -1, 90, 90, 90, 90)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_quadtrilateral_anglelessthanzero_all_int(self):
+        result = get_quadrilateral_type(2, 2, 2, 2, -5, -5, -5, -5)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_quadrilateral_anglegreaterthanoneeighty_all_int(self):
+        result = get_quadrilateral_type(2, 2, 2, 2, 190, 90, 90, 90)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_quadrilateral_square_all_float(self):
+        result = get_quadrilateral_type(2.4, 2.4, 2.4, 2.4, 90, 90, 90, 90)
+        self.assertEqual(result, 'square')
+
+    def test_get_quadrilateral_rectangle_all_int(self):
+        result = get_quadrilateral_type(2, 4, 2, 4, 90, 90, 90, 90)
+        self.assertEqual(result, 'rectangle')
+
+    def test_get_quadrilateral_rhombus_all_int(self):
+        result = get_quadrilateral_type(3, 3, 3, 3, 44, 136, 44, 136)
+        self.assertEqual(result, 'rhombus')
+
+    def test_get_quadrilateral_unequalsides_all_int(self):
+        result = get_quadrilateral_type(2, 2, 2, 4, 50, 130, 50, 130)
+        self.assertEqual(result, 'disconnected')
+
+    def test_get_quadrilateral_anglesnotequaltooneeighty_all_int(self):
+        result = get_quadrilateral_type(1, 2, 3, 4, 33, 146, 33, 146)
+        self.assertEqual(result, 'disconnected')
